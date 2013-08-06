85ae53efbd81741ffe840b638160fcafb8f9ad77
Orvid King <blah38621@gmail.com>
2013-08-06 14:23:14 -0500

    Added a Die() method which produces and alert and outputs the windows error code as well. This method is called if anything fails anywhere along the way. I also made it so that the process now acquires debug privileges. It still failes on EnumProcessModules though.

--- Injector/main.d
+++ Injector/main.d
@@ -19,32 +19,52 @@ extern(Windows)
 	}
 	alias THREADENTRY32* PTHREADENTRY32;
 	alias PTHREADENTRY32 LPTHREADENTRY32;
+	struct TOKEN_PRIVILEGES
+	{
+		DWORD PrivilegeCount;
+		LUID_AND_ATTRIBUTES Privileges[1];
+	}
+	alias TOKEN_PRIVILEGES* PTOKEN_PRIVILEGES;
+	alias PTOKEN_PRIVILEGES LPTOKEN_PRIVILEGES;
+	struct LUID_AND_ATTRIBUTES
+	{
+		LUID Luid;
+		DWORD Attributes;
+	}
+	struct LUID
+	{
+		DWORD LowPart;
+		LONG HighPart;
+	}
+	alias LUID* PLUID;
+	alias PLUID LPLUID;
 
+	BOOL AdjustTokenPrivileges(HANDLE TokenHandle, BOOL DisableAllPrivileges, PTOKEN_PRIVILEGES NewState, DWORD BufferLength, PTOKEN_PRIVILEGES PreviousState, PDWORD ReturnLength);
 	HANDLE CreateToolhelp32Snapshot(DWORD dwFlags, DWORD th32ProcessID);
-	BOOL EnumProcessModulesEx(HANDLE hProcess, HMODULE* lphModule, DWORD cb, LPDWORD lpcbNeeded, DWORD dwFilterFlag);
+	BOOL EnumProcessModules(HANDLE hProcess, HMODULE* lphModule, DWORD cb, LPDWORD lpcbNeeded);
 	HWND FindWindowA(LPCSTR lpClassName, LPCSTR lpWindowName);
 	DWORD GetModuleFileNameExA(HANDLE hProcess, HMODULE hModule, LPSTR lpFilename, DWORD nSize);
 	DWORD GetWindowThreadProcessId(HWND hWnd, LPDWORD lpdwProcessId);
+	BOOL LookupPrivilegeValueA(LPCTSTR lpSystemName, LPCTSTR lpName, PLUID lpLuid);
 	HANDLE OpenProcess(DWORD dwDesiredAccess, BOOL bInheritHandle, DWORD dwProcessId);
+	BOOL OpenProcessToken(HANDLE ProcessHandle, DWORD DesiredAccess, PHANDLE TokenHandle);
 	BOOL ReadProcessMemory(HANDLE hProcess, LPCVOID lpBaseAddress, LPVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesRead);
 	BOOL Thread32First(HANDLE hSnapshot, LPTHREADENTRY32 lpte);
 	BOOL Thread32Next(HANDLE hSnapshot, LPTHREADENTRY32 lpte);
 	BOOL WriteProcessMemory(HANDLE hProcess, LPCVOID lpBaseAddress, LPCVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesWritten);
 
 	immutable void* NULL = cast(void*)0;
-	enum
-	{
-		LIST_MODULES_32BIT = 0x01,
-	}
-	enum
-	{
-		TH32CS_SNAPTHREAD = 0x00000004,
-	}
+	enum SE_DEBUG_NAME = "SeDebugPrivilege";
+	enum SE_PRIVILEGE_ENABLE = 0x0002;
+	enum TOKEN_ADJUST_PRIVILEGES = 0x0020;
+	enum LIST_MODULES_32BIT = 0x01;
+	enum TH32CS_SNAPTHREAD = 0x00000004;
 	enum
 	{
 		PROCESS_VM_OPERATION = 0x0008,
 		PROCESS_VM_READ = 0x0010,
 		PROCESS_VM_WRITE = 0x0020,
+		PROCESS_QUERY_INFORMATION = 0x04000,
 	}
 	enum
 	{
@@ -94,8 +114,37 @@ extern(Windows)
 	];
 	immutable string SDKLocation = `C:\Tribes\TribesAscendSDK\TribesAscendSDK\bin\Debug\TribesAscendSDK.dll`;
 
+	void Die(const char* message)
+	{
+		printf("Last error code: %i\n", GetLastError());
+		MessageBoxA(NULL, message, message, MB_OK);
+	}
+
+	void AddTokenPrivilege(const char* privilegeName)
+	{
+		HANDLE processToken;
+		if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &processToken))
+			Die("Failed to aquire our process token!");
+		scope(exit)
+			CloseHandle(processToken);
+
+		TOKEN_PRIVILEGES privileges;
+		LUID luid;
+
+		if (!LookupPrivilegeValueA(cast(char*)NULL, privilegeName, &luid))
+			Die("Failed to lookup the luid of the debug privilege.");
+
+		privileges.PrivilegeCount = 1;
+		privileges.Privileges[0].Luid = luid;
+		privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLE;
+
+		if (!AdjustTokenPrivileges(processToken, FALSE, &privileges, privileges.sizeof, cast(PTOKEN_PRIVILEGES)NULL, cast(PDWORD)NULL))
+			Die("Failed to add the debug privilege!");
+	}
+
 	void main()
 	{
+		AddTokenPrivilege(SE_DEBUG_NAME);
 		HANDLE mainProcessHandle;
 		HANDLE mainThreadHandle = GetMainThreadHandle(&mainProcessHandle);
 		scope(exit)
@@ -106,54 +155,65 @@ extern(Windows)
 		SuspendThread(mainThreadHandle);
 		CONTEXT threadContext = { NULL };
 		threadContext.ContextFlags = CONTEXT_CONTROL | CONTEXT_INTEGER;
-		GetThreadContext(mainThreadHandle, &threadContext);
+		if (!GetThreadContext(mainThreadHandle, &threadContext))
+			Die("Failed to get the thread context!");
 
 		// Save the old stuff.
 		threadContext.Esp -= 4;
-		WriteProcessMemory(mainProcessHandle, cast(LPCVOID)threadContext.Esp, &threadContext.Eip, threadContext.Eip.sizeof, cast(SIZE_T*)NULL);
+		if (!WriteProcessMemory(mainProcessHandle, cast(LPCVOID)threadContext.Esp, &threadContext.Eip, threadContext.Eip.sizeof, cast(SIZE_T*)NULL))
+			Die("Failed to write the old eip to the stack!");
 		threadContext.Esp -= 4;
-		WriteProcessMemory(mainProcessHandle, cast(LPCVOID)threadContext.Esp, &threadContext.Eax, threadContext.Eax.sizeof, cast(SIZE_T*)NULL);
+		if (!WriteProcessMemory(mainProcessHandle, cast(LPCVOID)threadContext.Esp, &threadContext.Eax, threadContext.Eax.sizeof, cast(SIZE_T*)NULL))
+			Die("Failed to write the old eax to the stack!");
 		threadContext.Esp -= 4;
-		WriteProcessMemory(mainProcessHandle, cast(LPCVOID)threadContext.Esp, &threadContext.Ebx, threadContext.Ebx.sizeof, cast(SIZE_T*)NULL);
+		if (!WriteProcessMemory(mainProcessHandle, cast(LPCVOID)threadContext.Esp, &threadContext.Ebx, threadContext.Ebx.sizeof, cast(SIZE_T*)NULL))
+			Die("Failed to write the old ebx to the stack!");
 
 		LPVOID thunkAddress = VirtualAllocEx(mainProcessHandle, NULL, ThunkData.length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-		WriteProcessMemory(mainProcessHandle, thunkAddress, ThunkData.ptr, ThunkData.length, cast(SIZE_T*)NULL);
+		if (!WriteProcessMemory(mainProcessHandle, thunkAddress, ThunkData.ptr, ThunkData.length, cast(SIZE_T*)NULL))
+			Die("Failed to write the thunk to memory!");
 		threadContext.Eip = cast(uint)thunkAddress;
 
 		HMODULE loadedModules[1024];
 		DWORD cbNeeded;
-		EnumProcessModulesEx(mainProcessHandle, loadedModules.ptr, loadedModules.sizeof, &cbNeeded, LIST_MODULES_32BIT);
+		if (!EnumProcessModules(mainProcessHandle, loadedModules.ptr, loadedModules.sizeof, &cbNeeded))
+			Die("Failed to enumerate the process's modules!");
 		for (uint i = 0; i < cbNeeded / HMODULE.sizeof; i++)
 		{
 			CHAR moduleName[MAX_PATH];
 			moduleName[] = '\0';
-			GetModuleFileNameExA(mainProcessHandle, loadedModules[i], moduleName.ptr, moduleName.sizeof / CHAR.sizeof);
+			if (!GetModuleFileNameExA(mainProcessHandle, loadedModules[i], moduleName.ptr, moduleName.sizeof / CHAR.sizeof))
+				Die("Failed to get a module file name for a module loaded by the process!");
 			printf("Process has loaded '%s'\n", moduleName);
 			//if (!strcmp(moduleName, "Kernel32.dll"))
 		}
 		// TODO: Retrieve the correct address for LoadLibraryA in the running exe and set that as the value of ebx.
 
 		LPVOID stringAddress = VirtualAllocEx(mainProcessHandle, NULL, SDKLocation.length + 1, MEM_COMMIT, PAGE_READWRITE);
-		WriteProcessMemory(mainProcessHandle, cast(LPCVOID)stringAddress, SDKLocation.ptr, SDKLocation.length, cast(SIZE_T*)NULL);
+		if (!WriteProcessMemory(mainProcessHandle, cast(LPCVOID)stringAddress, SDKLocation.ptr, SDKLocation.length, cast(SIZE_T*)NULL))
+			Die("Failed to write the main SDK location string to memory!");
 		byte zero = 0;
-		WriteProcessMemory(mainProcessHandle, cast(LPCVOID)(stringAddress + SDKLocation.length), &zero, zero.sizeof, cast(SIZE_T*)NULL);
+		if (!WriteProcessMemory(mainProcessHandle, cast(LPCVOID)(stringAddress + SDKLocation.length), &zero, zero.sizeof, cast(SIZE_T*)NULL))
+			Die("Failed to write the null terminator of the SDK location string to memory!");
 		threadContext.Eax = cast(uint)stringAddress;
 
-		//SetThreadContext(mainThreadHandle, &threadContext);
-		ResumeThread(mainThreadHandle);
+		//if (!SetThreadContext(mainThreadHandle, &threadContext))
+		//	Die("Failed to set the thread context!");
+		if (!ResumeThread(mainThreadHandle))
+			Die("Failed to resume the thread!");
 	}
 
 	HANDLE GetMainThreadHandle(HANDLE* processHandle)
 	{
 		HWND wHand = FindWindowA(cast(char*)NULL, "Tribes: Ascend (32-bit, DX9)");
 		if (!wHand)
-			MessageBoxA(NULL, "Failed to get window handle!", "Failed to get window handle!", MB_OK);
+			Die("Failed to get window handle!");
 		DWORD procID;
 		if (!GetWindowThreadProcessId(wHand, &procID))
-			MessageBoxA(NULL, "Failed to get window thread process id!", "Failed to get window thread process id!", MB_OK);
-		HANDLE hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, procID);
+			Die("Failed to get window thread process id!");
+		HANDLE hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION | PROCESS_QUERY_INFORMATION, FALSE, procID);
 		if(!hProcess)
-			MessageBoxA(NULL, "Failed to get process handle!", "Failed to get process handle!", MB_OK);
+			Die("Failed to get process handle!");
 		*processHandle = hProcess;
 
 		DWORD threadID;
@@ -184,7 +244,7 @@ extern(Windows)
 		}
 
 		if (!threadID)
-			MessageBoxA(NULL, "Failed to get thread handle!", "Failed to get thread handle!", MB_OK);
+			Die("Failed to get thread handle!");
 
 		*processHandle = hProcess;
 
